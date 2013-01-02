ea417a869426b65d68d436c5276c8f581e0f9c87
Nick Booker <NMBooker@gmail.com>
2013-01-02 13:27:15 +0000

    Switch to logging module for reporting progress.

--- backup_script.py
+++ backup_script.py
@@ -4,6 +4,7 @@
 """
 
 import backup_conf
+import logging
 
 class BackupScript(object):
     """Top-down implementation of backup operation.
@@ -33,7 +34,7 @@ class BackupScript(object):
     def _cleanup_last_time(self):
         """Clean up any left-overs from last time.
         """
-        print "Cleanup from last time"
+        logging.info("Cleanup from last time")
 
     def _prepare_for_backup(self):
         """Do any backup preparations.
@@ -45,22 +46,22 @@ class BackupScript(object):
     def _make_lvm_snapshot(self):
         """Make the LVM snapshot.
         """
-        print "Make temporary LVM snapshot"
+        logging.info("Make temporary LVM snapshot")
 
     def _mount_lvm_snapshot(self):
         """Mount the LVM snapshot
         """
-        print "Mount temporary LVM snapshot"
+        logging.info("Mount temporary LVM snapshot")
 
     def _mount_binds(self):
         """Mount any bind mounts requested.
         """
-        print "Mount any bind mounts"
+        logging.info("Mount any bind mounts")
 
     def _do_backup(self):
         """Perform the backup copying operation itself.
         """
-        print "Do the backup copying operation"
+        logging.info("Do the backup copying operation")
 
     def _post_backup_cleanup(self):
         """Remove any temporary stuff from this backup.
@@ -72,14 +73,14 @@ class BackupScript(object):
     def _unmount_binds(self):
         """Unmount any bind mounts.
         """
-        print "Unmount any bind mounts"
+        logging.info("Unmount any bind mounts")
 
     def _unmount_lvm_snapshot(self):
         """Unmount any LVM snapshots.
         """
-        print "Unmount the temporary LVM snapshot"
+        logging.info("Unmount the temporary LVM snapshot")
 
     def _remove_lvm_snapshot(self):
         """Remove any LVM snapshots.
         """
-        print "Remove the temporary LVM snapshot"
+        logging.info("Remove the temporary LVM snapshot")
