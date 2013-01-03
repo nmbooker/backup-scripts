a30ddac1436bc25a3a254166942722969f4d2816
Nick Booker <NMBooker@gmail.com>
2013-01-03 15:30:14 +0000

    ArgList#append() now behaves as list#append() in case of no arguments.

--- backup_operation.py
+++ backup_operation.py
@@ -264,5 +264,15 @@ class IncrementalBackupStrategy(BaseBackupStrategy):
 
 
 class ArgList(list):
+    """A slightly extended version of list, with an append method that
+    can take more than one argument.
+
+    When building lists of arguments, this is syntatically cleaner than
+    using .extend(['--more', 'args']) or .append('--more') ; .append('args')
+    """
     def append(self, *args):
+        """Append the objects in the given args to the end of the list.
+        """
+        if not args:
+            raise TypeError('append() takes at least one argument (0 given)')
         self.extend(args)
