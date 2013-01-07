1e61668f8a1031a2f8f8508d1b95006656dc0259
Nick Booker <NMBooker@gmail.com>
2013-01-07 10:02:57 +0000

    Added internal logging to program_runners.py (this helped fix issue in previous commit)

--- program_runners.py
+++ program_runners.py
@@ -3,6 +3,7 @@
 """
 
 import subprocess
+import logging
 
 class LoggableCalls(object):
     """Log command line calls to a logger, report exit status if they fail.
@@ -13,6 +14,7 @@ class LoggableCalls(object):
     def __init__(self, logger, noop=False):
         self.log = logger
         self.noop = noop
+        self.internal_log = logging.getLogger(__name__)
 
     def check_call(self, cmd_args):
         """Log and optionally run
@@ -30,14 +32,18 @@ class LoggableCalls(object):
         """
         if not self.noop:
             self.really_run_cmd(cmd_args)
+        else:
+            self.internal_log.debug('noop set, not running command for real')
 
     def really_run_cmd(self, cmd_args):
         """Run command without checking for noop, and log error if it fails.
 
         The CalledProcessError is re-raised.
         """
+        self.internal_log.debug('running command for real...')
         try:
             subprocess.check_call(cmd_args)
         except subprocess.CalledProcessError, exc:
+            self.internal_log.debug('CalledProcessError caught')
             self.log.error(str(exc))
             raise exc
